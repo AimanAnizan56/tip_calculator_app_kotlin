330d08f64173a4138bd560781be46d8aefeec45b
Aiman Anizan <anizanaimann1@gmail.com>
2023-03-30 14:26:49 +0800

    Make method to display tip

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -6,7 +6,7 @@ import com.example.tipcalculator.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         val cost = binding.costOfService.text.toString().toDoubleOrNull()
 
         if (cost == null) {
-            binding.tipResult.text = ""
+            displayTip(0.0)
             return
         }
 
@@ -40,6 +40,10 @@ class MainActivity : AppCompatActivity() {
             tip = kotlin.math.ceil(tip) // use kotlin math to round up the tip
         }
 
+        displayTip(tip)
+    }
+
+    private fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip) // use java.text NumberFormat
 
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip) // set the calculated tip
